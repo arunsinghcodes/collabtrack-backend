9dc30cf17bd2538a6e4d5099b15a3f5dbb4c10ad
Arun Singh <arunsinghsnd@gmail.com>
2025-09-20 22:46:53 +0530

    server setup it done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Doing setup")
+import express from "express";
+
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req, res) =>{
+    res.status(200).send("Hello, Project Management API's");
+})
+
+app.listen(PORT,()=>{
+  console.log(`Server is running up at ${PORT}`)
+})
