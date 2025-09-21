44576f8a4dff7a617e9873f4d5bf89271fee7ddb
Arun Singh <arunsinghsnd@gmail.com>
2025-09-21 16:47:50 +0530

    completed the health check api end points

--- src/app.js
+++ src/app.js
@@ -1,9 +1,34 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.get("/", ()=>{
-    console.log("Welcome to Collab Track API's end points")
-})
+// Basic configuration
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// CORS configuration
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// import the routes
+
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
+app.get("/", (req, res) => {
+    res.status(200).send("This home for collab track backend servcies 😎")
+  console.log("Welcome to Collab Track API's end points");
+});
 
 export default app;
