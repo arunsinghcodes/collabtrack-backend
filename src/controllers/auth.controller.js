872e30c06de0476dc659783d55cd88c1a3c1ff6a
Arun Singh <arunsinghsnd@gmail.com>
2025-10-05 00:53:50 +0530

    sendEmailVerification auth controller done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -204,4 +204,43 @@ const verifyEmail = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, { isEmailVerified: true }, "Email is verified"));
 });
 
-export { registerUser, login, logoutUser, getCurrentUser, verifyEmail };
+const sendEmailVerification = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user?._id);
+
+  if (!user) {
+    throw new ApiError(404, "User does not exists!");
+  }
+
+  if (user.isEmailVerified) {
+    throw ApiError(409, "Email is already verified");
+  }
+
+  const { unHashedToken, hashedToken, tokenExpiry } =
+    user.generateTemporaryToken();
+
+  console.log(
+    `unHashedToken : ${unHashedToken}, hashedToken : ${hashedToken}, tokenExpiry:  ${tokenExpiry}`
+  );
+
+  user.emailVerificationToken = hashedToken;
+  user.emailVerificationExpiry = tokenExpiry;
+
+  await user.save({ validateBeforeSave: false });
+
+  await sendEmail({
+    email: user?.email,
+    subject: "Please verify your email",
+    mailgenContent: emailVerificationMailgenContent(
+      user.username,
+      `${req.protocol}://${req.get(
+        "host"
+      )}/api/v1/users/verify-email/${unHashedToken}`
+    ),
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Mail has been sent to your email Id"));
+});
+
+export { registerUser, login, logoutUser, getCurrentUser, verifyEmail, sendEmailVerification };
