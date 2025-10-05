a5495fd864e904d2b38c0baa053a1333eb229077
Arun Singh <arunsinghsnd@gmail.com>
2025-10-05 09:26:19 +0530

    changeCurrentPassword controller complted

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -359,7 +359,25 @@ const resetForgotPassword = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "Password reset successfully"));
 });
 
-const changeCurrentPassword = asyncHandler(async (req, res) => {});
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  const user = await User.findById(req.user?._id);
+
+  const isPasswordValid = await user.isPasswordCorrect(oldPassword);
+
+  if (!isPasswordValid) {
+    throw ApiError(400, "Invalid old password");
+  }
+
+  user.password = newPassword;
+
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password chnaged successfully"));
+});
 
 export {
   registerUser,
