485efcd64f43de7abf25827cc180bd15a25f6a7c
Arun Singh <arunsinghsnd@gmail.com>
2026-01-20 01:48:30 +0530

    Add: new api route end point getNoteById

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -1,6 +1,4 @@
-import mongoose, { Mongoose } from "mongoose";
 import { Project } from "../models/project.models.js";
-import { Task } from "../models/task.models.js";
 import { ApiError } from "../utils/api-error.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { asyncHandler } from "../utils/async-handler.js";
@@ -47,8 +45,25 @@ const createNote = asyncHandler(async (req, res) => {
 });
 
 const getNoteById = asyncHandler(async (req, res) => {
-  // getNote by Id
   const { projectId, notesId } = req.params;
+
+  const project = await Project.findById(projectId);
+  if (!project) {
+    throw new ApiError(404, "Project not found");
+  }
+
+  const note = await ProjectNote.findOne({
+    _id: notesId,
+    project: projectId,
+  }).populate("createdBY", "name email");
+
+  if (!note) {
+    throw new ApiError(404, "Note not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, note, "Note fetched successfully"));
 });
 
 const updateNote = asyncHandler(async (req, res) => {
